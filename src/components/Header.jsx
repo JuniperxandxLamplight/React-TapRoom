e893a326bd4602ffb87e09c70ddaa82e8e701dd5
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-04-19 13:55:40 -0700

    add images in header and restructure some webpack config

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import headerBackground from '../assets/images/darkWood.jpg';
 
 function Header(){
   return (
@@ -8,8 +9,12 @@ function Header(){
       <style jsx>{`
         div{
           width: 100%;
+          background-image: url(${headerBackground});
         }
         h1{
+          margin: 0;
+          font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif;
+          font-size: 3rem;
           text-align: center;
         }
       `}</style>

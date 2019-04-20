e6ec248086daa788330233513a84a431a5ecd603
Aurora Shido-Wagenet <fullmetalwoman@gmail.com>
2019-04-20 16:34:16 -0700

    add link styling and linking working

--- src/components/constants/Sidebar.jsx
+++ src/components/constants/Sidebar.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import background from "../../assets/images/sideWood.jpg";
 
+const linkStyle = {
+  textDecoration: 'none',
+  // marginLeft: '30px'
+}
+
 function Sidebar(){
   return(
     <div>
-      <Link to="/"><p>Inventory</p></Link>
-      <Link to="/order"><p>Order</p></Link>
+      <Link to="/" style={linkStyle}><p>Inventory</p></Link>
+      <Link to="/order" style={linkStyle}><p>Order</p></Link>
       <style jsx>{`
         div{
           float: left;
@@ -18,8 +23,17 @@ function Sidebar(){
           background-size: cover;
         }
         p{
+          font-size: 1.5rem;
           text-decoration: none;
           color: white;
+          margin: 10px;
+          padding: 10px;
+          border-radius: 5px;
+          border: 2px solid transparent;
+          transition: border .3s;
+        }
+        p:hover{
+          border: 2px solid white;
         }
       `}</style>
     </div>
